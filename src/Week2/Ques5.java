996ba4456ca9017a30159221a90a87712c6aad72
shrutisehgal07 <shruti.sehgal@coditas.com>
2026-02-08 12:43:44 +0530

    initial commit

--- src/Week2/Ques5.java
+++ src/Week2/Ques5.java
@@ -6,11 +6,77 @@ package Week2;
 	• Print values before and after method calls.
  */
 
+class Modification{                                  // class to pass the value for non primitive object 
+	private int value;
+	
+	Modification(int value){
+		this.value=value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value=value;
+	}
+	
+	
+}
+
+
 public class Ques5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Ques5 q = new Ques5();
+		
+		//passing the value of the primitive datatype
+		int num=10;
+		System.out.println("For primitive variable:");
+		System.out.println();
+		System.out.println("The value before the method call is = " + num);
+		
+		q.display(num);
+		
+		System.out.println("The value after the method call is = " + num);
 
+		
+		//passing the value of the non primitive data type
+		System.out.println();
+		
+		System.out.println("For non primitive object:");
+		System.out.println();
+		Modification m = new Modification(25);
+		System.out.println("The value before the method call is = " + m.getValue());
+		
+		q.display(m);
+		System.out.println("The value after the method call is =  " + m.getValue());
+			
 	}
+	
+
+	public void display(int number) {
+		System.out.println("The value inside method call is = " + number);
+
+		number = 28;
+
+		System.out.println("The value inside method call after modification is = " + number);
+	}
+	
+	public void display(Modification m) {
+		System.out.println("The value inside method call is =" + m.getValue());
+		
+		//m = new Modification(80);                         //for modifying only the value inside the method
+		m.setValue(100);									// for modifying the value on a class level
+		
+		/*on uncommenting both as the 'm' is given new value in method, when we setValue it refers to the local object reference 
+		 and changes the value of m inside method to 100 not changing the original, so after method call value will be 25 only.
+		*/
+		
+		System.out.println("The value inside method call after modification is = " + m.getValue());
+	}
+		
 
 }
+
