2dcbc50f734f303f42a7b0a4cd130230430175bb
shrutisehgal07 <shruti.sehgal@coditas.com>
2026-02-16 22:56:17 +0530

    adding

---
+++ src/Week3/Ques8/TrafficControl.java
@@ -0,0 +1,31 @@
+package Week3.Ques8;
+
+import java.util.concurrent.Semaphore;
+
+/*Create a traffic control system where only 3 cars can pass through a tunnel at
+the same time. Use a semaphore to limit the number of cars that can be in the tunnel
+concurrently.
+
+Objective: Learn how to use Semaphore to control access to a limited number of
+resources in a multithreaded environment.
+*/
+
+public class TrafficControl {
+
+	private final Semaphore semaphore = new Semaphore(3);
+	
+	public void pass() {
+		try {
+			semaphore.acquire(); // to acquire the lock 
+			System.out.println(Thread.currentThread().getName() + " has entered the tunnel");
+			Thread.sleep(2500); //considering the time it takes to pass the tunnel
+			System.out.println(Thread.currentThread().getName() + " has exited the tunnel");
+		} 
+		catch (InterruptedException e) {	      
+			e.printStackTrace();
+		}
+		finally {
+			semaphore.release();  //releasing the acquired lock
+		}
+	}
+}
---
+++ src/Week3/Ques8/Trigger.java
@@ -0,0 +1,25 @@
+package Week3.Ques8;
+
+/*Create a traffic control system where only 3 cars can pass through a tunnel at
+the same time. Use a semaphore to limit the number of cars that can be in the tunnel
+concurrently.
+
+Objective: Learn how to use Semaphore to control access to a limited number of
+resources in a multi threaded environment.
+*/
+
+public class Trigger {
+
+	public static void main(String[] args) {
+		
+		TrafficControl tc = new TrafficControl();
+		
+		for(int i=1;i<=5;i++) {
+			Thread car = new Thread(tc::pass, "Car " + i);
+			
+			car.start();
+		}
+
+	}
+
+}
